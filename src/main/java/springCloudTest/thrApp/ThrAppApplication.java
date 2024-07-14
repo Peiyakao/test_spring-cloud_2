d3746f3240d8fb509b804bb65ec9808a277cc083
Peiyakao <118188258+Peiyakao@users.noreply.github.com>
2024-07-14 16:58:08 +0800

    first commit

---
+++ src/main/java/springCloudTest/thrApp/ThrAppApplication.java
@@ -0,0 +1,15 @@
+package springCloudTest.thrApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ThrAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThrAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springCloudTest/thrApp/ThrAppApplicationTests.java
@@ -0,0 +1,13 @@
+package springCloudTest.thrApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThrAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
